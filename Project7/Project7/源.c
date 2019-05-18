2d2dbef02479ef8197bebff6a3802742894f9cb6
ei0 <1051214284@.qq.com>
2019-05-18 11:49:00 +0800

    提交代码

---
+++ Project7/Project7/源.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+//strcmp(str1,str2)str1>str2输出1;str1<str2输出-1;str1=str2输出0
+int main(){
+	/*char str1[21] = { 0 };
+	char str2[21] = "bitekeji" ;
+	int i, n;
+	for (n = 0; n < 3; n++){
+
+
+		for (i = 0; i < 21; i++){
+
+			scanf("%s",&str1[i]);
+		}
+		if (strcmp(str1, str2) == 0){
+			printf("成功登录");
+		}
+
+	}
+	printf("登录失败");*/
+
+	system("pause");
+	return 0;
+}
